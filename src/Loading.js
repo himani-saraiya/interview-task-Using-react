495c4abc501ccc2e52b81159195a5e02592b5240
Himani Saraiya <96934858+himani-saraiya@users.noreply.github.com>
2023-07-20 12:45:20 +0530

    Add files via upload

--- src/Loading.js
+++ src/Loading.js
@@ -3,8 +3,12 @@ import LoadingImage from './assests/Loading2.jpg';
 
 const LoadingComponent = () => {
   return (
-    <div>
-      <img src={LoadingImage} alt='Loading........' style={{height:"3bh", width:"5bh", alignItems:"center", display:"flex", justifyContent:"center"}}/>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <img
+        src={LoadingImage}
+        alt='Loading........'
+        style={{ height: '50px', width: '50px', margin: 'auto' }}
+      />
     </div>
   );
 };
